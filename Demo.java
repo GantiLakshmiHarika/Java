01bea2126a309a829ecd622e4cfc0f03a974eea7
GantiLakshmiHarika <gantiharika558@gmail.com>
2025-08-14 14:20:34 +0530

    Arraydemo

---
+++ Demo.java
@@ -0,0 +1,13 @@
+public class Demo {
+    public static void main(String[] args) {
+        // datatype[] arrayname = new datatype[size];
+        int[] nums = new int[5];  // size 5 → indexes 0 to 4
+  
+        // storing values
+        nums[0] = 10;
+        nums[1] = 20;
+        nums[2] = 30;
+        nums[4] = 106;
+        System.out.println(nums[4]);
+    }
+}
